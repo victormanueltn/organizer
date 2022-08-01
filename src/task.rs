67d42557fc4825bac285286e4fd36158440e31f9
Victor Trejo <victormanueltn@gmail.com>
2022-08-01 21:59:03 +0200

    Add unit tests for Tasks's descriptions.

--- src/task.rs
+++ src/task.rs
@@ -10,11 +10,29 @@ impl Task {
             description,
         }
     }
+
     pub fn completed(&self) -> bool {
         self.task_completed
     }
 
     pub fn set_completed(&mut self, completed: bool) {
         self.task_completed = completed;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn correct_task_description() {
+        let task = Task::new("This is a test task".to_string());
+        assert_eq!("This is a test task", task.description);
+    }
+
+    #[test]
+    fn empty_task_description() {
+        let task = Task::new("".to_string());
+        assert_eq!("", task.description);
     }
 }
