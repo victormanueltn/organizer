de4cb2832161473e61d6a3a579ea5ffd6c9abee7
Victor Trejo <victormanueltn@gmail.com>
2022-08-12 18:01:16 +0200

    Add button for adding task.

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,7 @@ pub enum Message {
     EditingTask,
     TextInput(String),
     DescriptionEdited,
+    AddTask,
 }
 
 impl Sandbox for Organizer {
@@ -35,6 +36,8 @@ impl Sandbox for Organizer {
 
         let return_row = add_task_view(&self.tasks[0], a_row);
 
+        let return_row = add_task_button(self, return_row);
+
         return_row.into()
     }
 
@@ -44,6 +47,9 @@ impl Sandbox for Organizer {
             Message::EditingTask => self.tasks[0].set_state(TaskState::BeingEdited),
             Message::TextInput(text) => self.tasks[0].edit(text),
             Message::DescriptionEdited => self.tasks[0].set_state(TaskState::Idle),
+            Message::AddTask => self
+                .tasks
+                .push(Task::new("Yet another task for Victor!".to_string())),
         }
     }
 }
@@ -84,4 +90,19 @@ fn add_task_view<'a>(a_task: &Task, a_row: Row<'a, Message>) -> Row<'a, Message>
                 .push(a_text_input)
         }
     }
+}
+
+fn add_task_button<'a>(organizer: &Organizer, a_row: Row<'a, Message>) -> Row<'a, Message> {
+    let create_task_text = Text::new("Add a task for Victor.")
+        .width(Length::Units(60))
+        .horizontal_alignment(alignment::Horizontal::Center)
+        .size(20);
+    let edit_button = button(create_task_text)
+        .on_press(Message::AddTask)
+        .padding(10);
+
+    a_row
+        .spacing(20)
+        .align_items(iced::Alignment::Center)
+        .push(edit_button)
 }
