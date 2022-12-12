7b6c5d680d38aab5604db32878a4c90de2d6cc4c
Victor Trejo <victormanueltn@gmail.com>
2022-12-12 22:03:29 +0100

    Improve error handling for data.save and data.load.

--- src/data.rs
+++ src/data.rs
@@ -1,20 +1,47 @@
 use crate::task::Task;
 use serde::{Deserialize, Serialize};
-use std::error::Error;
 
 #[derive(Serialize, Deserialize, PartialEq, Debug)]
 pub(crate) struct Data {
     pub(crate) tasks: Vec<Task>,
 }
 
-impl Data {
-    fn _save(&self, file_name: &str) -> Result<(), Box<dyn Error>> {
-        let serialized_data = serde_json::to_string(&self)?;
-        std::fs::write(file_name, serialized_data)?;
-        Ok(())
+#[derive(Debug)]
+pub(crate) struct FileError {
+    message: String,
+    kind: FileErrorKind,
+}
+
+#[derive(Debug)]
+enum FileErrorKind {
+    Load,
+    Serialization,
+    Write,
+}
+
+impl From<serde_json::Error> for FileError {
+    fn from(original_error: serde_json::Error) -> Self {
+        FileError {
+            message: original_error.to_string(),
+            kind: FileErrorKind::Serialization,
+        }
     }
-    fn _load(file_name: &str) -> Result<Data, Box<dyn Error>> {
-        let serialized_data = std::fs::read_to_string(file_name)?;
+}
+
+impl Data {
+    fn save(&self, file_name: &str) -> Result<(), FileError> {
+        let serialized_data = serde_json::to_string(&self).unwrap();
+
+        std::fs::write(file_name, serialized_data).map_err(|_| FileError {
+            message: "Problem saving file.".to_string(),
+            kind: FileErrorKind::Write,
+        })
+    }
+    fn load(file_name: &str) -> Result<Data, FileError> {
+        let serialized_data = std::fs::read_to_string(file_name).map_err(|_| FileError {
+            message: "Problem loading file".to_string(),
+            kind: FileErrorKind::Load,
+        })?;
         let data = serde_json::from_str::<Self>(&serialized_data)?;
         Ok(data)
     }
@@ -27,12 +54,33 @@ mod tests {
     #[test]
     fn save_and_load_returns_same_data() {
         let data = Data {
-            tasks: vec![Task::new(0_usize)],
+            tasks: vec![Task::new(0_usize), Task::new(1_usize)],
         };
 
-        data._save("data.json");
-        let loaded_data = Data::_load("data.json").unwrap();
+        data.save("data.json").unwrap();
+        let loaded_data = Data::load("data.json").unwrap();
 
         assert_eq!(data, loaded_data);
+    }
+
+    #[test]
+    fn load_inexistent_file() {
+        let loaded_data = Data::load("inexsistent.json");
+
+        assert!(matches!(loaded_data.unwrap_err().kind, FileErrorKind::Load));
+    }
+
+    #[test]
+    fn save_to_inexistent_folder() {
+        let data = Data {
+            tasks: vec![Task::new(0_usize), Task::new(1_usize)],
+        };
+
+        let save_result = data.save("./inexistent_directory/data.json");
+
+        assert!(matches!(
+            save_result.unwrap_err().kind,
+            FileErrorKind::Write
+        ));
     }
 }
