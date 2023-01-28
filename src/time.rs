1833c16dfd0da64223b400ae07560f18a8ffa81a
Victor Trejo <victormanueltn@gmail.com>
2023-01-28 21:06:33 +0100

    Implement std::ops::{sub, div} for Duration.

--- src/time.rs
+++ src/time.rs
@@ -8,6 +8,15 @@ struct Time {
     time: DateTime<Local>,
 }
 
+#[derive(Debug, PartialEq)]
+struct Duration(chrono::Duration);
+
+impl Duration {
+    fn new(minutes: i64) -> Self {
+        Self(chrono::Duration::minutes(minutes))
+    }
+}
+
 impl fmt::Display for Time {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}", self.time.to_rfc2822())
@@ -18,6 +27,23 @@ impl Time {
     fn new(time: &str) -> Time {
         Time {
             time: DateTime::parse_from_rfc2822(time).unwrap().into(),
+        }
+    }
+}
+
+impl std::ops::Sub<Self> for Time {
+    type Output = Duration;
+    fn sub(self, rhs: Self) -> Self::Output {
+        Duration(self.time - rhs.time)
+    }
+}
+
+impl std::ops::Div<Self> for Duration {
+    type Output = f32;
+    fn div(self, rhs: Self) -> Self::Output {
+        match rhs.0.num_seconds() {
+            0 => panic!(),
+            _ => self.0.num_seconds() as f32 / rhs.0.num_seconds() as f32,
         }
     }
 }
@@ -36,6 +62,8 @@ impl Serialize for Time {
 
 #[cfg(test)]
 mod tests {
+    use float_cmp::ApproxEq;
+
     use super::*;
 
     #[test]
@@ -50,5 +78,40 @@ mod tests {
         let before = Time::new("Sat, 21 Jan 2023 12:25:20 +0100");
         let after = Time::new("Sat, 21 Jan 2023 12:25:21 +0100");
         assert!(after > before);
+    }
+
+    #[test]
+    fn equality() {
+        let before = Time::new("Sat, 21 Jan 2023 12:25:20 +0100");
+        let after = Time::new("Sat, 21 Jan 2023 13:25:20 +0100");
+        let duration_1 = after - before;
+        let duration_2 = Duration::new(60);
+        assert_eq!(duration_1, duration_2);
+    }
+
+    #[test]
+    fn duration_ratio() {
+        let duration_1 = Duration::new(30);
+        let duration_2 = Duration::new(15);
+        let ratio = duration_1 / duration_2;
+        assert!(float_cmp::approx_eq!(f32, ratio, 2.0));
+
+        let duration_1 = Duration::new(30);
+        let duration_2 = Duration::new(14);
+        let ratio = duration_1 / duration_2;
+        assert!(float_cmp::approx_eq!(f32, ratio, 2.142857));
+
+        let duration_1 = Duration::new(0);
+        let duration_2 = Duration::new(14);
+        let ratio = duration_1 / duration_2;
+        assert!(float_cmp::approx_eq!(f32, ratio, 0.));
+    }
+
+    #[test]
+    #[should_panic]
+    fn divide_by_zero_panics() {
+        let duration_1 = Duration::new(1);
+        let duration_2 = Duration::new(0);
+        let _ = duration_1 / duration_2;
     }
 }
