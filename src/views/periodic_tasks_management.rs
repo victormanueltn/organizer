dfd050b10d8286b897196a99760f4fd8db87e665
Victor Trejo <victormanueltn@gmail.com>
2023-07-22 08:53:39 +0200

    Revert "Change toolchain."
    This reverts commit c7ecefc804a20f8ae13b713b432456609f012ff4.

--- src/views/periodic_tasks_management.rs
+++ src/views/periodic_tasks_management.rs
@@ -27,8 +27,4 @@ impl PeriodicTasksManagementView for Organizer {
             Message::SelectView(value) => self.view_type = Some(value),
         }
     }
-
-
-    // - Initial date (and time?)
-    // - Frequency (x times per day/week/month/year)
 }
